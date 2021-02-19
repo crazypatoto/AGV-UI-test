11aeb65f4669329c460f7ae206925fc21c2df4d8
crazypatoto <crazypatoto@gmail.com>
2021-02-19 22:35:01 +0800

    semi-OK

--- AGV/Form1.Designer.cs
+++ AGV/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -38,6 +39,12 @@
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView_AGV = new ListViewNF();
+            this.columnHeader_hostName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_IPA = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_state = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.panel1.SuspendLayout();
@@ -47,27 +54,25 @@
             // 
             this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetPartial;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.79281F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.20719F));
             this.tableLayoutPanel1.Controls.Add(this.pictureBox, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1414, 879);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1060, 703);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // pictureBox
             // 
             this.pictureBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox.Location = new System.Drawing.Point(7, 7);
-            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox.Location = new System.Drawing.Point(6, 6);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1045, 865);
+            this.pictureBox.Size = new System.Drawing.Size(695, 691);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
@@ -75,24 +80,24 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.listView_AGV);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.button_Clear);
             this.panel1.Controls.Add(this.button_Send);
             this.panel1.Controls.Add(this.listView1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Enabled = false;
-            this.panel1.Location = new System.Drawing.Point(1063, 7);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(710, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(344, 865);
+            this.panel1.Size = new System.Drawing.Size(344, 691);
             this.panel1.TabIndex = 1;
             // 
             // button_Clear
             // 
             this.button_Clear.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button_Clear.Location = new System.Drawing.Point(4, 452);
-            this.button_Clear.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Clear.Location = new System.Drawing.Point(3, 362);
             this.button_Clear.Name = "button_Clear";
-            this.button_Clear.Size = new System.Drawing.Size(232, 85);
+            this.button_Clear.Size = new System.Drawing.Size(174, 68);
             this.button_Clear.TabIndex = 2;
             this.button_Clear.Text = "Clear";
             this.button_Clear.UseVisualStyleBackColor = true;
@@ -101,10 +106,9 @@
             // button_Send
             // 
             this.button_Send.Font = new System.Drawing.Font("微軟正黑體", 12F);
-            this.button_Send.Location = new System.Drawing.Point(4, 551);
-            this.button_Send.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Send.Location = new System.Drawing.Point(3, 441);
             this.button_Send.Name = "button_Send";
-            this.button_Send.Size = new System.Drawing.Size(232, 85);
+            this.button_Send.Size = new System.Drawing.Size(174, 68);
             this.button_Send.TabIndex = 1;
             this.button_Send.Text = "Send";
             this.button_Send.UseVisualStyleBackColor = true;
@@ -118,10 +122,9 @@
             this.columnHeader3,
             this.columnHeader4});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(-3, 8);
-            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Location = new System.Drawing.Point(161, 325);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(235, 436);
+            this.listView1.Size = new System.Drawing.Size(177, 350);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -141,13 +144,58 @@
             // 
             this.columnHeader4.Text = "Y";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 551);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView_AGV
+            // 
+            this.listView_AGV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader_hostName,
+            this.columnHeader_IPA,
+            this.columnHeader_state});
+            this.listView_AGV.HideSelection = false;
+            this.listView_AGV.Location = new System.Drawing.Point(3, 3);
+            this.listView_AGV.Name = "listView_AGV";
+            this.listView_AGV.Size = new System.Drawing.Size(338, 350);
+            this.listView_AGV.TabIndex = 4;
+            this.listView_AGV.UseCompatibleStateImageBehavior = false;
+            this.listView_AGV.Click += new System.EventHandler(this.listView_AGV_Click);
+            // 
+            // columnHeader_hostName
+            // 
+            this.columnHeader_hostName.Text = "Hostname";
+            this.columnHeader_hostName.Width = 80;
+            // 
+            // columnHeader_IPA
+            // 
+            this.columnHeader_IPA.Text = "IP Address";
+            this.columnHeader_IPA.Width = 120;
+            // 
+            // columnHeader_state
+            // 
+            this.columnHeader_state.Text = "Current State";
+            this.columnHeader_state.Width = 100;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 16;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1414, 879);
+            this.ClientSize = new System.Drawing.Size(1060, 703);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "AGV Test";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -170,6 +218,12 @@
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.Button button_Send;
         private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.ListView listView_AGV;
+        private System.Windows.Forms.ColumnHeader columnHeader_hostName;
+        private System.Windows.Forms.ColumnHeader columnHeader_IPA;
+        private System.Windows.Forms.ColumnHeader columnHeader_state;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- AGV/Form1.cs
+++ AGV/Form1.cs
@@ -15,24 +15,9 @@ using System.Threading;
 
 namespace AGV
 {
+
     public partial class Form1 : Form
     {
-        public class QRCode
-        {
-            public QRCode(Int16 x, Int16 y, UInt32 tagNum)
-            {
-                X = x;
-                Y = y;
-                TagNum = tagNum;
-            }
-
-            public Int16 X { get; }
-            public Int16 Y { get; }
-            public UInt32 TagNum { get; }
-
-            public override string ToString() => $"{X},{Y},{TagNum}";
-        }
-
         List<QRCode> QRCodes = new List<QRCode>();
         List<QRCode> Route = new List<QRCode>();
         Int32 OriginOffset_X = 400;
@@ -46,6 +31,11 @@ namespace AGV
         Int16 odmAngle = 0;
         bool isRunning = false;
 
+        AGVHandler AGVhandler;
+        int pickedAGVIndex = -1;
+        int prevAGVCount = 0;
+        QRCode pickedQR = null;
+
         public Form1()
         {
             loadQRCode();
@@ -58,7 +48,27 @@ namespace AGV
             listView1.FullRowSelect = true;
             listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
 
-            Thread t = new Thread(() => { AGVConnect(); });
+            listView_AGV.View = View.Details;
+            listView_AGV.GridLines = true;
+            listView_AGV.FullRowSelect = true;
+            //listView_AGV.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+            //Thread t = new Thread(() => { AGVConnect(); });
+            //t.IsBackground = true;
+            //t.Start();
+
+            TcpListener serverTcpListener = new TcpListener(IPAddress.Any, 6666);
+            AGVhandler = new AGVHandler(serverTcpListener);
+
+            panel1.Enabled = true;
+
+            Thread t = new Thread(() =>
+            {
+                while (true)
+                {
+                    AGVhandler.AcceptNewConnection();
+                }
+            });
             t.IsBackground = true;
             t.Start();
 
@@ -122,6 +132,8 @@ namespace AGV
                                 odmY = binaryReader.ReadInt16();
                                 odmAngle = binaryReader.ReadInt16();
 
+                                Console.WriteLine($"X = {odmX}, Y = {odmY}, Angle = {odmAngle}");
+
                                 if (isRunning)
                                 {
                                     if (Route.Count > 1)
@@ -183,7 +195,9 @@ namespace AGV
         {
             drawQR(e.Graphics);
             drawRoute(e.Graphics);
-            drawAGV(e.Graphics);
+            //drawAGV(e.Graphics);
+            drawAGVs(e.Graphics);
+            drawAGVRoutes(e.Graphics);
         }
 
         private void updateListView()
@@ -210,16 +224,17 @@ namespace AGV
             foreach (QRCode qr in QRCodes)
             {
                 //g.DrawEllipse(p, qr.X/5, -qr.Y/5, 100, 100);
-                if (Route.Contains(qr))
+                if (pickedQR == qr && pickedAGVIndex >= 0)
                 {
-                    p.Color = Color.Green;
-                    drawBrush.Color = Color.Green;
+                    p.Color = AGVhandler.AGVList[pickedAGVIndex].color;
+                    drawBrush.Color = AGVhandler.AGVList[pickedAGVIndex].color;
                 }
                 else
                 {
                     p.Color = Color.Black;
                     drawBrush.Color = Color.Black;
                 }
+
                 g.DrawRectangle(p, (qr.X * positionScale) + OriginOffset_X - 15, (-qr.Y * positionScale) + OriginOffset_Y - 15, 30, 30);
                 g.DrawEllipse(p, (qr.X * positionScale) + OriginOffset_X - 3, (-qr.Y * positionScale) + OriginOffset_Y - 3, 6, 6);
                 g.DrawString($"Tag {qr.TagNum}", drawFont, drawBrush, (qr.X * positionScale) + OriginOffset_X - 16, (-qr.Y * positionScale) + OriginOffset_Y + 16);
@@ -244,6 +259,27 @@ namespace AGV
             }
         }
 
+        private void drawAGVs(Graphics g)
+        {
+            foreach (var agv in AGVhandler.AGVList)
+            {
+                Pen p = new Pen(agv.color, 3);
+                Pen p2 = new Pen(agv.color, 3);
+                p2.EndCap = LineCap.RoundAnchor;
+                p2.EndCap = LineCap.ArrowAnchor;
+
+                //Console.WriteLine($"{agv.hostName}: X = {agv.odmX}, Y = {agv.odmY}, Angle = {agv.odmAngle}");
+
+                float x = (agv.odmX * positionScale) + OriginOffset_X;
+                float y = (-agv.odmY * positionScale) + OriginOffset_Y;
+
+                g.DrawEllipse(p, x - 30, y - 30, 60, 60);
+                g.DrawLine(p2, x, y, (float)(x + Math.Cos(agv.odmAngle / 180.0 * Math.PI) * 60.0f), (float)(y - Math.Sin(agv.odmAngle / 180.0 * Math.PI) * 60.0f));
+                g.DrawString(agv.hostName, new Font("Arial", 8), new SolidBrush(agv.color), new PointF(x, y - 50));
+            }
+
+        }
+
         private void drawRoute(Graphics g)
         {
             Pen p = new Pen(Color.Green, 5);
@@ -253,6 +289,21 @@ namespace AGV
             for (int i = 0; i < Route.Count - 1; i++)
             {
                 g.DrawLine(p, Route[i].X * positionScale + OriginOffset_X, -Route[i].Y * positionScale + OriginOffset_Y, Route[i + 1].X * positionScale + OriginOffset_X, -Route[i + 1].Y * positionScale + OriginOffset_Y);
+            }
+        }
+
+        private void drawAGVRoutes(Graphics g)
+        {
+            foreach (var agv in AGVhandler.AGVList)
+            {
+                Pen p = new Pen(agv.color, 5);
+                p.EndCap = LineCap.RoundAnchor;
+                p.EndCap = LineCap.ArrowAnchor;
+
+                for (int i = 0; i < agv.Route.Count - 1; i++)
+                {
+                    g.DrawLine(p, agv.Route[i].X * positionScale + OriginOffset_X, -agv.Route[i].Y * positionScale + OriginOffset_Y, agv.Route[i + 1].X * positionScale + OriginOffset_X, -agv.Route[i + 1].Y * positionScale + OriginOffset_Y);
+                }
             }
         }
 
@@ -286,16 +337,25 @@ namespace AGV
                     {
                         if (Route.Count == 0 || Route.Last() != qr)
                         {
-                            Route.Add(qr);
+                            if (pickedAGVIndex >= 0)
+                            {
+                                pickedQR = qr;
+                                AGVhandler.AGVList[pickedAGVIndex].Route.Add(qr);
+                            }
+                            //Route.Add(qr);
                         }
                     }
                     else if (e.Button == MouseButtons.Right)
                     {
-                        if (Route.Contains(qr))
-                        {
-                            Route.RemoveAt(Route.FindLastIndex((QRCode _qr) => { return _qr.TagNum == qr.TagNum; }));
-                        }
 
+                        if (pickedAGVIndex >= 0)
+                        {
+                            if (AGVhandler.AGVList[pickedAGVIndex].Route.Contains(qr))
+                            {
+                                AGVhandler.AGVList[pickedAGVIndex].Route.RemoveAt(AGVhandler.AGVList[pickedAGVIndex].Route.FindLastIndex((QRCode _qr) => { return _qr.TagNum == qr.TagNum; }));
+                                //Route.RemoveAt(Route.FindLastIndex((QRCode _qr) => { return _qr.TagNum == qr.TagNum; }));
+                            }
+                        }
                     }
                     break;
                 }
@@ -305,6 +365,256 @@ namespace AGV
         }
 
         private void button_Send_Click(object sender, EventArgs e)
+        {
+            //string str = "";
+
+            //if (Route.Count > 0)
+            //{
+            //    str += $"{Route[0].TagNum},{Route[0].X},{Route[0].Y}";
+            //}
+            //for (int i = 1; i < Route.Count; i++)
+            //{
+            //    str += $";{Route[i].TagNum},{Route[i].X},{Route[i].Y}";
+            //}
+            ////MessageBox.Show(str);
+            //binaryWriter.Write(Encoding.UTF8.GetBytes(str));
+            //isRunning = true;
+            //button_Send.Enabled = false;
+
+            if (pickedAGVIndex >= 0)
+            {
+                pickedQR = null;                
+                AGVhandler.AGVList[pickedAGVIndex].sendRoute();
+                pickedAGVIndex = -1;
+            }
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            if(pickedAGVIndex >= 0)
+            {
+                AGVhandler.AGVList[pickedAGVIndex].Route.Clear();
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox.Invalidate();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            if (AGVhandler.AGVList.Count != prevAGVCount)
+            {
+                prevAGVCount = AGVhandler.AGVList.Count;
+                listView_AGV.Items.Clear();
+                foreach (var agv in AGVhandler.AGVList)
+                {
+                    ListViewItem lvitem = new ListViewItem(agv.hostName);
+                    lvitem.SubItems.Add(((IPEndPoint)agv.tcpClient.Client.RemoteEndPoint).Address.ToString());
+                    if (agv.isRunning)
+                    {
+                        lvitem.SubItems.Add("Running...");
+                    }
+                    else
+                    {
+                        lvitem.SubItems.Add("Idle");
+                    }
+                    listView_AGV.Items.Add(lvitem);
+                }
+            }
+
+            for (int i = 0; i < listView_AGV.Items.Count; i++)
+            {
+                ListViewItem item = listView_AGV.Items[i];
+                if (AGVhandler.AGVList[i].isRunning)
+                {
+                    item.SubItems[2].Text = "Running...";
+                }
+                else
+                {
+                    item.SubItems[2].Text = "Idle";
+                }
+            }
+            pictureBox.Invalidate();
+        }
+
+        private void listView_AGV_Click(object sender, EventArgs e)
+        {
+            pickedQR = null;
+            if (listView_AGV.SelectedItems.Count > 0)
+            {
+                pickedAGVIndex = listView_AGV.SelectedItems[0].Index;
+            }
+
+        }
+    }
+
+    public class AGVHandler
+    {
+        private TcpListener tcpListener;
+
+        public AGVHandler(TcpListener _listener)
+        {
+            tcpListener = _listener;
+        }
+
+        public List<AGV> AGVList = new List<AGV>();
+        public int AGVCount { get; private set; } = 0;
+
+        public void AcceptNewConnection()
+        {
+            TcpClient client;
+
+            lock (this)
+            {
+                Console.WriteLine("Accepting new client!!");
+                tcpListener.Start();
+                tcpListener.Start();
+                client = tcpListener.AcceptTcpClient();
+                tcpListener.Stop();
+            }
+
+            AGV agv = new AGV(
+                GetHostName(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()),
+                client,
+                new BinaryReader(client.GetStream()),
+                new BinaryWriter(client.GetStream())
+                );
+            AGVList.Add(agv);
+            AGVCount++;
+            Console.WriteLine($"{agv.hostName} is Connected!");
+
+            HandleAGV(agv);
+        }
+
+        private void HandleAGV(AGV agv)
+        {
+
+            Thread t = new Thread(() =>
+            {
+                while (agv.tcpClient.Connected)
+                {
+                    try
+                    {
+                        agv.odmX = agv.clientBinaryReader.ReadInt16();
+                        agv.odmY = agv.clientBinaryReader.ReadInt16();
+                        agv.odmAngle = agv.clientBinaryReader.ReadInt16();
+
+                        //Console.WriteLine($"X = {agv.odmX}, Y = {agv.odmY}, Angle = {agv.odmAngle}");
+                    }
+                    catch (Exception ex)
+                    {
+                        //Console.WriteLine(ex);
+                        break;
+                    }
+
+                    if (agv.isRunning)
+                    {
+                        if (agv.Route.Count > 1)
+                        {
+                            QRCode qr = agv.Route[1];
+                            if (new Rectangle((int)(qr.X - 50), (int)(qr.Y - 50), 100, 100).Contains((int)(agv.odmX), (int)(agv.odmY)))
+                            {
+                                agv.Route.RemoveAt(0);
+                            }
+                        }
+                        else
+                        {
+                            QRCode qr = agv.Route[0];
+                            if (new Rectangle((int)(qr.X - 50), (int)(qr.Y - 50), 100, 100).Contains((int)(agv.odmX), (int)(agv.odmY)))
+                            {
+                                agv.Route.RemoveAt(0);
+                            }
+                            agv.isRunning = false;
+                        }
+
+                    }
+                }
+                Console.WriteLine($"\r\nClient {agv.hostName} is Disconnected!");
+                AGVList.Remove(agv);
+                AGVCount--;
+            });
+
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        private string GetHostName(string ipAddress)
+        {
+            try
+            {
+                Console.WriteLine(ipAddress);
+                IPHostEntry entry = Dns.GetHostEntry(ipAddress);
+
+                if (entry != null)
+                {
+                    Console.WriteLine(entry.HostName);
+                    return entry.HostName;
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return "";
+        }
+    }
+
+    public class QRCode
+    {
+        public QRCode(Int16 x, Int16 y, UInt32 tagNum)
+        {
+            X = x;
+            Y = y;
+            TagNum = tagNum;
+        }
+
+        public Int16 X { get; }
+        public Int16 Y { get; }
+        public UInt32 TagNum { get; }
+        public override string ToString() => $"{X},{Y},{TagNum}";
+    }
+
+    public class AGV
+    {
+        public AGV(string _hostName, TcpClient _tcpClient, BinaryReader _clientBinaryReader, BinaryWriter _clientBinaryWriter)
+        {
+            hostName = _hostName;
+            hostName = hostName.Replace(".local", "");
+            tcpClient = _tcpClient;
+            clientBinaryReader = _clientBinaryReader;
+            clientBinaryWriter = _clientBinaryWriter;
+
+            switch (hostName)
+            {
+                case "AGV001":
+                    color = Color.Red;
+                    break;
+                case "AGV002":
+                    color = Color.Green;
+                    break;
+                case "AGV003":
+                    color = Color.Blue;
+                    break;
+                default:
+                    color = Color.Black;
+                    break;
+            }
+        }
+        public string hostName { get; private set; } = "";
+        public TcpClient tcpClient { get; private set; }
+        public BinaryReader clientBinaryReader { get; private set; }
+        public BinaryWriter clientBinaryWriter { get; private set; }
+        public List<QRCode> Route { get; set; } = new List<QRCode>();
+        public Int16 odmX { get; set; }
+        public Int16 odmY { get; set; }
+        public Int16 odmAngle { get; set; }
+        public Color color { get; set; } = Color.Black;
+        public bool isRunning { get; set; } = false;
+
+        public void sendRoute()
         {
             string str = "";
 
@@ -317,16 +627,30 @@ namespace AGV
                 str += $";{Route[i].TagNum},{Route[i].X},{Route[i].Y}";
             }
             //MessageBox.Show(str);
-            binaryWriter.Write(Encoding.UTF8.GetBytes(str));
+            clientBinaryWriter.Write(Encoding.UTF8.GetBytes(str));
             isRunning = true;
-            button_Send.Enabled = false;
+        }
+    }
+
+    class ListViewNF : System.Windows.Forms.ListView
+    {
+        public ListViewNF()
+        {
+            //Activate double buffering
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+
+            //Enable the OnNotifyMessage event so we get a chance to filter out 
+            // Windows messages before they get to the form's WndProc
+            this.SetStyle(ControlStyles.EnableNotifyMessage, true);
         }
 
-        private void button_Clear_Click(object sender, EventArgs e)
+        protected override void OnNotifyMessage(Message m)
         {
-            Route.Clear();
-            pictureBox.Invalidate();
-            updateListView();
+            //Filter out the WM_ERASEBKGND message
+            if (m.Msg != 0x14)
+            {
+                base.OnNotifyMessage(m);
+            }
         }
     }
 }
